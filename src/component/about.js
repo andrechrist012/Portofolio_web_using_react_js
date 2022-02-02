3c1b0270543e26fd58067b8a7027a8eee48b1d6b
andrechrist012 <andrechrist.p06@gmail.com>
2022-02-02 20:19:17 +0700

    create responsive web

--- src/component/about.js
+++ src/component/about.js
@@ -9,7 +9,7 @@ const About = () => {
             <div className="about" id='about'> 
                 <img src={Image} alt="" width="140px" className="clouldLeft"/>
                 <p className="introduce1">
-                    <span style={{fontSize:'85px', color : '#728ED7'}}>Hi There,</span><br/>
+                    <span className='welcome'>Hi There,</span><br/>
                     I’m a <span style={{color : '#728ED7'}}>Student</span> at<br/>
                     BINUS University
                 </p>
@@ -19,15 +19,10 @@ const About = () => {
                     I'm a computer science student at BINUS.
                     
                 </p>
-                <br/><br/><br/>
-                <a href="#personality" style={{
-                        color : '#728ED7', 
-                        fontSize: '30px', 
-                        textDecoration: 'none',
-                        marginLeft: '6.5em'
-                    }}>
+                
+                <a href="#personality" className='LearnMore'>
                     <b>Learn more</b><br/>
-                    <svg style={{marginLeft: '8.5em'}} stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.2" baseProfile="tiny" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M18.414 10.656c-.781-.781-2.047-.781-2.828 0l-1.586 1.586v-7.242c0-1.105-.896-2-2-2-1.105 0-2 .895-2 2v7.242l-1.586-1.586c-.781-.781-2.047-.781-2.828 0s-.781 2.047 0 2.828l6.414 6.414 6.414-6.414c.781-.781.781-2.046 0-2.828z"></path>
+                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.2" baseProfile="tiny" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M18.414 10.656c-.781-.781-2.047-.781-2.828 0l-1.586 1.586v-7.242c0-1.105-.896-2-2-2-1.105 0-2 .895-2 2v7.242l-1.586-1.586c-.781-.781-2.047-.781-2.828 0s-.781 2.047 0 2.828l6.414 6.414 6.414-6.414c.781-.781.781-2.046 0-2.828z"></path>
                     </svg>
                 </a>
             </div>
--- src/component/message.js
+++ src/component/message.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style/message.css'
 import Image from './image/Vector.png'
 
 const Message = () => {
+    const initialState = {
+        name : '',
+        email : '',
+        description : ''
+    }
+
+    const [input, setInput] = useState({...initialState})
+
+    const handleChange = (event) => {
+        const value = event.target.value
+        const typeOfInput = event.target.name
+        setInput({...input, [typeOfInput] : value})
+    }
+
+    const handleSumbit = () => {
+        if(input.name === '' || input.email === '' || input.description === ''){
+            return false
+        }
+        else if (input){
+            alert('message send')
+            return true
+        }
+    }
 
     return (
         <>  
@@ -12,17 +35,17 @@ const Message = () => {
                 <div className="messageForm">                   
                     <p>Send Me a Message</p>
                     <div className="line"></div>
-                    <form action="https://formsubmit.co/andrechrist.p06@gmail.com" method="POST">
+                    <form action="https://formsubmit.co/andrechrist.p06@gmail.com" method="POST" >
                         <input type="hidden" name="_subject" value="New email from Your website"></input>
                         <input type="hidden" name="_captcha" value="false"></input>
                         <div className="left">
-                            <input type="text" placeholder="name" required name='name' />
-                            <input type="email" placeholder="email" required name='email' />
+                            <input type="text" placeholder="name" required name='name' value={input.name} onChange={handleChange} />
+                            <input type="email" placeholder="email" required name='email' value={input.email} onChange={handleChange} />
                         </div>
                         <div className="right">
-                            <textarea name="message" id="" cols="30" rows="10" placeholder="Your message" required></textarea>
+                            <textarea name="description" id="" cols="30" rows="10" placeholder="message" required value={input.description} onChange={handleChange}></textarea>
                             <input type="hidden" name="_next" value="https://andre-christian.netlify.app/"></input>
-                            <button type="submit" className="sendMessage">Send</button>
+                            <button type="submit" className="sendMessage" onClick={handleSumbit}>Send</button>
                         </div>
                     </form>
                 </div>
